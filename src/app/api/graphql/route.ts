92eda4b98c5d9c533d8e6345d395793a2d7766d1
meet <meetparmar2362004@gmail.com>
2025-11-21 17:00:58 +0530

    graphql implimented

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
+import { NextRequest } from "next/server";
 
 const server = new ApolloServer({
   typeDefs,
@@ -10,7 +11,9 @@ const server = new ApolloServer({
 
 const handler = startServerAndCreateNextHandler(server);
 
-export { handler as POST };
+export async function POST(req: NextRequest) {
+  return handler(req);
+}
 
 export async function GET() {
   return new Response("GraphQL endpoint is live", { status: 200 });
